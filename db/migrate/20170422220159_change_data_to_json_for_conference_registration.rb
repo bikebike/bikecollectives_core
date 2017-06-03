96d317f3f21312c3534c2266da8b71c734a5cf9c
Godwin <goodgodwin@hotmail.com>
2017-06-03 13:11:59 -0700

    Added error handing for data migration

--- db/migrate/20170422220159_change_data_to_json_for_conference_registration.rb
+++ db/migrate/20170422220159_change_data_to_json_for_conference_registration.rb
@@ -1,16 +1,31 @@
 require 'json'
+require 'yaml'
 
 class ChangeDataToJsonForConferenceRegistration < ActiveRecord::Migration
   def change
     data = {}
     ConferenceRegistration.where.not(data: nil).each do |registration|
-      data[registration.id] = JSON.parse(registration.data)
+      begin
+        data[registration.id] = JSON.parse(registration.data)
+      rescue
+        begin
+          data[registration.id] = YAML.parse(registration.data)
+        rescue
+          puts "Error parsing #{registration.data}"
+        end
+      end
     end
     remove_column :conference_registrations, :data
     add_column :conference_registrations, :data, :json
     ConferenceRegistration.where(id: data.keys).each do |registration|
-      registration.data = data[registration.id]
-      registration.save
+      if data[registration.id].present?
+        begin
+          registration.data = data[registration.id]
+          registration.save
+        rescue
+          puts "Eror saving data as JSON #{data[registration.id]}"
+        end
+      end
     end
   end
 end
